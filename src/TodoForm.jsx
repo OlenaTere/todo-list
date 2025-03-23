ede71da39908f3d607ca51c99c134dc55fa4a68a
Olena Tereshchenko <olenatereshchenko123@gmail.com>
2025-03-22 20:18:04 -0400

    created a form for submitting new todos

---
+++ src/TodoForm.jsx
@@ -0,0 +1,10 @@
+
+export default function TodoForm(){
+    return (
+    <form>
+        <label htmlFor="todoTitle">Todo</label>
+        <input id="todoTitle" />
+        <button>Add Todo</button>
+    </form>
+);
+}
